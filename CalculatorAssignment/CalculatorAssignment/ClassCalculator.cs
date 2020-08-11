07b0804eebcb016f3f5d111a40edf218891989ae
dotnet-sandipan-july-2020 <68274705+dotnet-sandipan-july-2020@users.noreply.github.com>
2020-08-11 21:54:09 +0530

    calculator lib file and cs code

---
+++ CalculatorAssignment/CalculatorAssignment/ClassCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorAssignment
+{
+    public class ClassCalculator
+    {
+       
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+        public int Add()
+        {
+            return Num1 + Num2;
+        }
+        public int Substract()
+        {
+            return Num1-Num2;
+        }
+        public int Multiply()
+        {
+            return Num1*Num2;
+        }
+        public int Divide()
+        {
+            return Num1/Num2;
+        }
+    }
+}
