b66e43839569c91ce6ee0f07df57b43ab49494d7
Sandipan Ghosh <sandipan.g@zensar.com>
2020-08-11 21:52:18 +0530

    windowsFormApps_Assignment_calculator

---
+++ WindowsFormsCalculator/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace WindowsFormsCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lblAdd = new System.Windows.Forms.Label();
+            this.lblSub = new System.Windows.Forms.Label();
+            this.lblMul = new System.Windows.Forms.Label();
+            this.lblDiv = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.btnSub = new System.Windows.Forms.Button();
+            this.btnMul = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " Number 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(84, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = " Number 2";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(155, 236);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lblAdd
+            // 
+            this.lblAdd.AutoSize = true;
+            this.lblAdd.Location = new System.Drawing.Point(155, 304);
+            this.lblAdd.Name = "lblAdd";
+            this.lblAdd.Size = new System.Drawing.Size(0, 17);
+            this.lblAdd.TabIndex = 8;
+            // 
+            // lblSub
+            // 
+            this.lblSub.AutoSize = true;
+            this.lblSub.Location = new System.Drawing.Point(334, 304);
+            this.lblSub.Name = "lblSub";
+            this.lblSub.Size = new System.Drawing.Size(0, 17);
+            this.lblSub.TabIndex = 11;
+            // 
+            // lblMul
+            // 
+            this.lblMul.AutoSize = true;
+            this.lblMul.Location = new System.Drawing.Point(487, 304);
+            this.lblMul.Name = "lblMul";
+            this.lblMul.Size = new System.Drawing.Size(0, 17);
+            this.lblMul.TabIndex = 12;
+            // 
+            // lblDiv
+            // 
+            this.lblDiv.AutoSize = true;
+            this.lblDiv.Location = new System.Drawing.Point(634, 304);
+            this.lblDiv.Name = "lblDiv";
+            this.lblDiv.Size = new System.Drawing.Size(0, 17);
+            this.lblDiv.TabIndex = 13;
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(337, 87);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 22);
+            this.txtNum1.TabIndex = 14;
+            this.txtNum1.TextChanged += new System.EventHandler(this.txtNum1_TextChanged);
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(337, 134);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 22);
+            this.txtNum2.TabIndex = 15;
+            this.txtNum2.TextChanged += new System.EventHandler(this.txtNum2_TextChanged);
+            // 
+            // btnSub
+            // 
+            this.btnSub.Location = new System.Drawing.Point(354, 236);
+            this.btnSub.Name = "btnSub";
+            this.btnSub.Size = new System.Drawing.Size(75, 23);
+            this.btnSub.TabIndex = 16;
+            this.btnSub.Text = "Sub";
+            this.btnSub.UseVisualStyleBackColor = true;
+            this.btnSub.Click += new System.EventHandler(this.btnSub_Click_1);
+            // 
+            // btnMul
+            // 
+            this.btnMul.Location = new System.Drawing.Point(490, 235);
+            this.btnMul.Name = "btnMul";
+            this.btnMul.Size = new System.Drawing.Size(75, 23);
+            this.btnMul.TabIndex = 17;
+            this.btnMul.Text = "Mul";
+            this.btnMul.UseVisualStyleBackColor = true;
+            this.btnMul.Click += new System.EventHandler(this.btnMul_Click_1);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Location = new System.Drawing.Point(657, 234);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(75, 23);
+            this.btnDiv.TabIndex = 18;
+            this.btnDiv.Text = "Div";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnMul);
+            this.Controls.Add(this.btnSub);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.lblDiv);
+            this.Controls.Add(this.lblMul);
+            this.Controls.Add(this.lblSub);
+            this.Controls.Add(this.lblAdd);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label lblAdd;
+        private System.Windows.Forms.Label lblSub;
+        private System.Windows.Forms.Label lblMul;
+        private System.Windows.Forms.Label lblDiv;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Button btnSub;
+        private System.Windows.Forms.Button btnMul;
+        private System.Windows.Forms.Button btnDiv;
+    }
+}
+
---
+++ WindowsFormsCalculator/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CalculatorAssignment;
+
+namespace WindowsFormsCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ClassCalculator calc= new ClassCalculator();
+        
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            calc.Num1 = Convert.ToInt32(txtNum1.Text);
+            calc.Num2 = Convert.ToInt32(txtNum2.Text);
+            lblAdd.Text= "Addition :" +calc.Add().ToString();
+           
+        }
+
+        private void btnSub_Click(object sender, EventArgs e)
+        {
+
+           
+        }
+
+        private void btnMul_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void txtNum1_TextChanged(object sender, EventArgs e)
+        {
+            
+           
+        }
+
+        private void txtNum2_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnSub_Click_1(object sender, EventArgs e)
+        {
+            calc.Num1 = Convert.ToInt32(txtNum1.Text);
+            calc.Num2 = Convert.ToInt32(txtNum2.Text);
+            lblAdd.Text = "Substraction :" + calc.Substract().ToString();
+        }
+
+        private void btnMul_Click_1(object sender, EventArgs e)
+        {
+            calc.Num1 = Convert.ToInt32(txtNum1.Text);
+            calc.Num2 = Convert.ToInt32(txtNum2.Text);
+
+            lblAdd.Text = "Multiplication :" + calc.Multiply().ToString();
+        }
+
+        private void btnDiv_Click_1(object sender, EventArgs e)
+        {
+            calc.Num1 = Convert.ToInt32(txtNum1.Text);
+            calc.Num2 = Convert.ToInt32(txtNum2.Text);
+
+            lblAdd.Text = "Divison" + calc.Divide().ToString();
+        }
+    }
+}
